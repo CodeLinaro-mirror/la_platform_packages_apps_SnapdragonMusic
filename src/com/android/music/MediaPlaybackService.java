bba09973a097471dd09ac096c3930aff944e0d2f
Y Mehta <ymehta@codeaurora.org>
2010-07-30 20:21:21 +0530

    packages/apps/Music: Fix to handle prev and next commands on Music app when the playlist contain invalid content
    - The current logic to store number of track failed to open, and adjusting the
    the track number accordingly on PREV command, do not handle all the scenarios
    of NEXT and PREV.
    
    - This fix makes sure that if previous track is failed to open, then it will try
    to open track before previous and but not the next to failed track.
    
    Change-Id: I7ca57b84379ea47779366365094f4f6fa03f9dd6
    CRs-fixed: 227622, 225345

--- src/com/android/music/MediaPlaybackService.java
+++ src/com/android/music/MediaPlaybackService.java
@@ -118,6 +118,7 @@ public class MediaPlaybackService extends Service {
     private static final String LOGTAG = "MediaPlaybackService";
     private final Shuffler mRand = new Shuffler();
     private int mOpenFailedCounter = 0;
+    private static boolean mPlayPrev = false;
     String[] mCursorCols = new String[] {
             "audio._id AS _id",             // index must match IDCOLIDX below
             MediaStore.Audio.Media.ARTIST,
@@ -1034,7 +1035,11 @@ public class MediaPlaybackService extends Service {
                 stop(true);
                 if (mOpenFailedCounter++ < 10 &&  mPlayListLen > 1) {
                     // beware: this ends up being recursive because next() calls open() again.
+                    if(mPlayPrev == true) {
+                        prev();
+                    } else {
                     next(false);
+                    }
                 }
                 if (! mPlayer.isInitialized() && mOpenFailedCounter != 0) {
                     // need to make sure we only shows this once
@@ -1194,6 +1199,7 @@ public class MediaPlaybackService extends Service {
                 play();
                 return;
             }
+            mPlayPrev = true;
             if (mShuffleMode == SHUFFLE_NORMAL) {
                 // go to previously-played track and remove it from the history
                 int histsize = mHistory.size();
@@ -1240,7 +1246,7 @@ public class MediaPlaybackService extends Service {
             if (mHistory.size() > MAX_HISTORY_SIZE) {
                 mHistory.removeElementAt(0);
             }
-
+            mPlayPrev = false;
             if (mShuffleMode == SHUFFLE_NORMAL) {
                 // Pick random next track from the not-yet-played ones
                 // TODO: make it work right after adding/removing items in the queue.
