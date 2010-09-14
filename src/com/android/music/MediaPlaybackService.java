fcb8c98f1d397c9ce943078645a9ea29c0b74741
Y Mehta <ymehta@codeaurora.org>
2010-09-14 11:33:36 +0530

    Revert "platform/packages/apps/Music: Corrupted QCElP clip lead to skip on next clip"
    This reverts commit 24d6226db770da79b28fb0d00e28ec07f82b4df2.
    
    Change-Id: Iaee59c69a3db7951e7fb046d843a9b5e90216fd9
    CRs-Fixed: 254299, 254521

--- src/com/android/music/MediaPlaybackService.java
+++ src/com/android/music/MediaPlaybackService.java
@@ -1821,6 +1821,7 @@ public class MediaPlaybackService extends Service {
                     mMediaPlayer.release();
                     mMediaPlayer = new MediaPlayer();
                     Toast.makeText(MediaPlaybackService.this, R.string.playback_failed, Toast.LENGTH_SHORT).show();
+                    next(false);
                     break;
                 }
                 return false;
