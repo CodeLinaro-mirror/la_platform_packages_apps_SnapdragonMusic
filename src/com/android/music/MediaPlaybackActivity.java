ab49a2ede1f128f92cf60862523f27ecbec9f11f
Yogesh Sadhu <ysadhu@codeaurora.org>
2011-01-24 18:28:15 -0800

    packages/apps/Music: Fix for auto resume of Music after Voice call
    -Music app is getting resumed once voice call ends even if user has paused
     while Voice call is in progess.
    -Add a state variable to keep a track if user has paused it.
    
    (cherry picked from commit d9637d5aee21b276b51d8e52e8aba1872756446c)
    CRs-Fixed: 271436
    Change-Id: I040104ebc02675a7f122dbd2bb32c648462deb55

--- src/com/android/music/MediaPlaybackActivity.java
+++ src/com/android/music/MediaPlaybackActivity.java
@@ -967,6 +967,7 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
         try {
             if(mService != null) {
                 if (mService.isPlaying()) {
+                    MediaPlaybackService.mPausedByTransientLossOfFocus = false;
                     mService.pause();
                 } else {
                     mService.play();
