55576d42c69889ea0bf0907b72c50a68639db154
Vicky Sehrawat <vickys@codeaurora.org>
2012-10-30 14:46:08 -0700

    Music: Fix RepeatAll disable bug on last clip
    - Disabling RepeatAll on the last clip gave an
      error message at the end of the clip.
    - The mNextMediaPlayer was not being set to NULL,
      when the repeatAll was disabled on the last
      clip.
    - Set mNextMediaPlayer to null.
    CRs-fixed: 407444
    
    Change-Id: Idef907bd665df8b42e0d06d581c83f2e15357f46

--- src/com/android/music/MediaPlaybackService.java
+++ src/com/android/music/MediaPlaybackService.java
@@ -1093,6 +1093,9 @@ public class MediaPlaybackService extends Service {
                 mPlayer.setNextDataSource(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI + "/" + id);
             }
         }
+        else {
+            mPlayer.setNextDataSource(null);
+        }
     }
 
     /**
