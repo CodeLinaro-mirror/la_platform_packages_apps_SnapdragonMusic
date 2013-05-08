c43a60651cdbe43240bfc69b8447385ac08d2208
tsima <quic_tsima@codeaurora.org>
2013-05-08 10:25:53 +0800

    Music: Fix the song you add into "playlist 1" is not in the list,but can play by tapping "Next" icon.
    - Because related activity doesn't call addToPlaylist() when add a song to the playlist.
    - Add MusicUtils.addToPlaylist() to related activity.
    
    Change-Id: Id77cbe6bc468195cfee51316a58929e4416d297c
    CRs-Fixed: BZ1960

--- src/com/android/music/TrackBrowserActivity.java
+++ src/com/android/music/TrackBrowserActivity.java
@@ -697,6 +697,7 @@ public class TrackBrowserActivity extends ListActivity
             case QUEUE: {
                 long [] list = new long[] { mSelectedId };
                 MusicUtils.addToCurrentPlaylist(this, list);
+                MusicUtils.addToPlaylist(this, list, MusicUtils.getPlayListId());
                 return true;
             }
 
@@ -905,6 +906,10 @@ public class TrackBrowserActivity extends ListActivity
                 } catch (RemoteException ex) {
                 }
             }
+        }
+
+        if (mEditMode && !mPlaylist.equals("nowplaying")) {
+            MusicUtils.setPlayListId(Long.valueOf(mPlaylist));
         }
         MusicUtils.playAll(this, mTrackCursor, position);
     }
