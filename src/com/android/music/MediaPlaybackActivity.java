82d153e338642126955512c9a6ae1418d8e53f5b
shaochunyu <shaochunyu@hisensecom.com>
2013-05-13 15:27:22 +0800

    Playing time can not be larger than Duration

--- src/com/android/music/MediaPlaybackActivity.java
+++ src/com/android/music/MediaPlaybackActivity.java
@@ -1198,6 +1198,9 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
         try {
             long pos = mPosOverride < 0 ? mService.position() : mPosOverride;
             if ((pos >= 0) && (mDuration > 0)) {
+            	if(pos > mDuration){
+            		pos = mDuration;
+            	}
                 mCurrentTime.setText(MusicUtils.makeTimeString(this, pos / 1000));
                 int progress = (int) (1000 * pos / mDuration);
                 if(!mFromTouch){
