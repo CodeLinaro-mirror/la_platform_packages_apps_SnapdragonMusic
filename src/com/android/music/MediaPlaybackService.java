55fd42000591d899e7b4a58c4328535ba94321df
shaochunyu <shaochunyu@hisensecom.com>
2013-05-24 13:40:22 +0800

    MSM8230-246:can't play music when press next button on music widget when the nowplaying list is empty

--- src/com/android/music/MediaPlaybackService.java
+++ src/com/android/music/MediaPlaybackService.java
@@ -1583,10 +1583,10 @@ public class MediaPlaybackService extends Service {
 
     public void gotoNext(boolean force) {
         synchronized (this) {
-            if (mPlayListLen <= 0) {
+           /* if (mPlayListLen <= 0) {
                 Log.d(LOGTAG, "No play queue");
                 return;
-            }
+            }*/
 
             int pos = getNextPosition(force);
             if (pos < 0) {
