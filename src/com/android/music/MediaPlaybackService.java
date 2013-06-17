957084ac05876f0563311b43ce15440110bf33ac
tsima <quic_tsima@codeaurora.org>
2013-06-17 14:14:02 +0800

    Music: Fix will play music according to old order when change playlist order
    - After moving the music track in playlist, the next track is not updated
    - Update the next track after moving music track in playlist
    
    (cherry picked from commit I53e20390473e3e3f658abe0242e51deeb87eb4de)
    CRs-Fixed: 492329
    
    Change-Id: Ia56e932b3552f483556f5dacc36917353cff9644

--- src/com/android/music/MediaPlaybackService.java
+++ src/com/android/music/MediaPlaybackService.java
@@ -1140,6 +1140,9 @@ public class MediaPlaybackService extends Service {
                         mPlayPos++;
                 }
             }
+            if (index2 == mNextPlayPos || index1 == mNextPlayPos) {
+                setNextTrack();
+            }
             notifyChange(QUEUE_CHANGED);
         }
     }
