81387a3bfc88c7c6abaf39a421cc5d0350aa3e8a
tsima <quic_tsima@codeaurora.org>
2013-06-18 13:35:38 +0800

    Music: Fix the music state will display play after force stop it.
    - Music appwidget state is not updated when music is forced stop
    - Update the music appwidget state when music is forced stop
    
    Change-Id: Ia924573c355ef1aa33e8a67d664f2fa61bef2424
    CRs-Fixed: 492365

--- src/com/android/music/MediaPlaybackService.java
+++ src/com/android/music/MediaPlaybackService.java
@@ -112,6 +112,7 @@ public class MediaPlaybackService extends Service {
          "com.qualcomm.music.playersettingsresponse";
     private static final String EXTRA_SHUFFLE_VAL = "shuffle";
     private static final String EXTRA_REPEAT_VAL = "repeat";
+    private static final String UPDATE_WIDGET_ACTION = "com.android.music.updatewidget";
 
     private static final int TRACK_ENDED = 1;
     private static final int RELEASE_WAKELOCK = 2;
@@ -504,6 +505,11 @@ public class MediaPlaybackService extends Service {
             unregisterReceiver(mA2dpReceiver);
             mA2dpReceiver = null;
         }
+
+        //notify music widget update state
+        //because of service is died, so can not use performUpdate
+        Intent intent = new Intent(UPDATE_WIDGET_ACTION);
+        sendBroadcast(intent);
 
         mWakeLock.release();
         super.onDestroy();
