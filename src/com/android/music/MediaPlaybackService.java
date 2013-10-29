de3fd50ad53f9055c36a00b5d45769802baaad5a
Naresh Tanniru <ntanniru@codeaurora.org>
2013-10-29 17:48:47 +0530

    apps/Music: Media Playback service check on Music app
    - During random testing some times music app is
      crashing if we start after long time
    
    - On restart of music app, Music utils interface
      communicates with music service to identify last played
      track, which can result crash if service is stopped
      by Garbage collector due to weak reference
    
    - Changed service to activity communication object instance
      from weak reference to strong ref.
      Added null pointer check condition to make sure no system
      crash even if some one stops mediaservice forcibly
      from settings app
    
    Change-Id: Ica2dd0166ba4be80413a82881ea2769ecbaf4182

--- src/com/android/music/MediaPlaybackService.java
+++ src/com/android/music/MediaPlaybackService.java
@@ -57,7 +57,7 @@ import android.widget.Toast;
 import java.io.FileDescriptor;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.ref.WeakReference;
+import java.lang.ref.SoftReference;
 import java.util.Random;
 import java.util.Vector;
 
@@ -502,6 +502,7 @@ public class MediaPlaybackService extends Service {
 
         mWakeLock.release();
         super.onDestroy();
+        mServiceInUse = false;
     }
     
     private final char hexdigits [] = new char [] {
@@ -1936,7 +1937,7 @@ public class MediaPlaybackService extends Service {
      */
     public long getAudioId() {
         synchronized (this) {
-            if (mPlayPos >= 0 && mPlayer.isInitialized()) {
+            if (mPlayPos >= 0 && mPlayer!=null && mPlayer.isInitialized()) {
                 return mPlayList[mPlayPos];
             }
         }
@@ -2551,10 +2552,11 @@ public class MediaPlaybackService extends Service {
      * has a remote reference to the stub.
      */
     static class ServiceStub extends IMediaPlaybackService.Stub {
-        WeakReference<MediaPlaybackService> mService;
+        //changing weak ref to softref to prevent media playercrash
+        SoftReference<MediaPlaybackService> mService;
         
         ServiceStub(MediaPlaybackService service) {
-            mService = new WeakReference<MediaPlaybackService>(service);
+            mService = new SoftReference<MediaPlaybackService>(service);
         }
 
         public void openFile(String path)
