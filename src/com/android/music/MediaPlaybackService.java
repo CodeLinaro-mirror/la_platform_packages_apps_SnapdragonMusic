383bbd7d31a328d9913adbbbea8ab2230dd91888
wjiang <wjiang@codeaurora.org>
2013-12-20 13:10:23 +0800

    Music: Fix Prompt "Unfortunately,music has stopped" when click party shuffle while repeating current song.
    - It is caused by the code of setNextTrack(). If getNextPosition()
    returns -1, that means there's no next song left.
    - If there's no next song left, set it null to release mNextMediaPlayer.
    
    Change-Id: If2ff58dc877741e9ada50dc06eb61090d602122c
    CRs-Fixed: 534521

--- src/com/android/music/MediaPlaybackService.java
+++ src/com/android/music/MediaPlaybackService.java
@@ -1241,6 +1241,8 @@ public class MediaPlaybackService extends Service {
             if (mNextPlayPos >= 0) {
                 long id = mPlayList[mNextPlayPos];
                 mPlayer.setNextDataSource(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI + "/" + id);
+            } else {
+                mPlayer.setNextDataSource(null);
             }
         } else {
             mPlayer.setNextDataSource(null);
