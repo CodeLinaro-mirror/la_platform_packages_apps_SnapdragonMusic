7a619a555f7132ba5d20dc1315560d2f079de8f5
wjiang <wjiang@codeaurora.org>
2013-12-26 15:26:47 +0800

    Music: Avoid AudioPreviw force close
    mPlayer has been released when rotating screen, so mPlayer.seek()
    will threw null pointer exception. Check it before seeking to.
    
    CRs-Fixed: 502641
    Change-Id: I841f2179b44ac65bbbce6350fe4d07308ac20821

--- src/com/android/music/AudioPreview.java
+++ src/com/android/music/AudioPreview.java
@@ -411,7 +411,9 @@ public class AudioPreview extends Activity implements OnPreparedListener, OnErro
             mSeekStopPosition = progress;
         }
         public void onStopTrackingTouch(SeekBar bar) {
-            mPlayer.seekTo(mSeekStopPosition);
+            if (mPlayer != null) {
+                mPlayer.seekTo(mSeekStopPosition);
+            }
             mSeeking = false;
         }
     };
