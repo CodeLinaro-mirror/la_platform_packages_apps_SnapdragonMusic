0ecda08fa6fe9f093e6eaa353483692954419a75
wjiang <wjiang@codeaurora.org>
2014-01-23 17:07:28 +0800

    Music: Fix seekbar does not update when interrupting by call
    In this case when the target gets the call, the seek event is posted
    with repcnt as -1, mService as NULL. Since mService is null it returns
    without setting mPosoverride to -1. Due to this, mPosoverride retains
    the older value. Set mPosoverride to -1 and add it both in method
    scanForward and scanBackward.
    
    Change-Id: Ibffef233e6d77a3699d28f80267e81de5de2381c

--- src/com/android/music/MediaPlaybackActivity.java
+++ src/com/android/music/MediaPlaybackActivity.java
@@ -975,7 +975,11 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
     }
     
     private void scanBackward(int repcnt, long delta) {
-        if(mService == null) return;
+        if (mService == null) {
+            if (repcnt < 0)
+                mPosOverride = -1;
+            return;
+        }
         try {
             if(repcnt == 0) {
                 mStartSeekPos = mService.position();
@@ -1014,7 +1018,11 @@ public class MediaPlaybackActivity extends Activity implements MusicUtils.Defs,
     }
 
     private void scanForward(int repcnt, long delta) {
-        if(mService == null) return;
+        if (mService == null) {
+           if (repcnt < 0)
+               mPosOverride = -1;
+           return;
+        }
         try {
             if(repcnt == 0) {
                 mStartSeekPos = mService.position();
