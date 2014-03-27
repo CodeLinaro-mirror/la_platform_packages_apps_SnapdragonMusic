abdae7a2912377eb7ceda6e68a4bf4fdc03d17ae
kunleiz <quic_kunleiz@codeaurora.org>
2014-03-27 19:08:24 +0800

    Music: The supported file's status isn't right after playing an unsupported clip. -The play status isn't right after playing an unsupported clip. -Add a Error message to pause the playback.
    Change-Id: Ib8f450fe1cdc350e159721c9d0a5d9194b39f656
    CRs-Fixed: 629599

--- src/com/android/music/MediaPlaybackService.java
+++ src/com/android/music/MediaPlaybackService.java
@@ -2523,6 +2523,7 @@ public class MediaPlaybackService extends Service {
                         mHandler.sendMessageDelayed(mHandler.obtainMessage(ERROR),0);
                         return true;
                     }
+                    mHandler.sendMessageDelayed(mHandler.obtainMessage(ERROR),0);
                     break;
                 }
                 return false;
