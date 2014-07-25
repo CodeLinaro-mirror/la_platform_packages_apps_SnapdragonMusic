08ec0a111f623b1286a215df95c42a9b86db9817
kaiyiz <kaiyiz@codeaurora.org>
2014-07-25 09:40:06 +0800

    Music: fix first music name display abnormally
    Colour of first music name changed when drag up and down repeatedly.
    
    Assign colour of text as before when bindView.
    
    CRs-fixed: 695251
    
    Change-Id: I5da867ca4721a39e71cf6916b694595a6db2f3bb

--- src/com/android/music/TrackBrowserActivity.java
+++ src/com/android/music/TrackBrowserActivity.java
@@ -1615,6 +1615,8 @@ public class TrackBrowserActivity extends ListActivity
             
             cursor.copyStringToBuffer(mTitleIdx, vh.buffer1);
             vh.line1.setText(vh.buffer1.data, 0, vh.buffer1.sizeCopied);
+            // set textview color as original color "@android:color/bright_foreground_dark"
+            vh.line1.setTextColor(0xffffffff);
             
             int secs = cursor.getInt(mDurationIdx) / 1000;
             if (secs == 0) {
@@ -1641,6 +1643,8 @@ public class TrackBrowserActivity extends ListActivity
             }
             builder.getChars(0, len, vh.buffer2, 0);
             vh.line2.setText(vh.buffer2, 0, len);
+            // set textview color as original color "@android:color/dim_foreground_dark"
+            vh.line2.setTextColor(0xffbebebe);
 
             ImageView iv = vh.play_indicator;
             long id = -1;
