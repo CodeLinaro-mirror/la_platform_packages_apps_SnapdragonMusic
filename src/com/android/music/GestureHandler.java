1cd322d60c76672f61b28918c8e3b3aad723fd6e
Jing (Mia) Wang <kaiyiz@codeaurora.org>
2014-12-19 14:50:32 +0800

    Music: Start MediaPlaybackService failed when first started
    The first time to draw gesture the mediaplaybackservice start failed
    so it can't to play music.
    
    Corresponding to the action to send the command to mediaplaybackservice
    and start service to play music.
    
    CRs-fixed: 772368
    
    Change-Id: I2502dce009d61abba24a9a10c30c5b25453db9bf

--- src/com/android/music/GestureHandler.java
+++ src/com/android/music/GestureHandler.java
@@ -27,68 +27,46 @@
  */
 package com.android.music;
 
-import com.android.music.MusicUtils.ServiceToken;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.ComponentName;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.os.RemoteException;
 import android.util.Log;
 
-public class GestureHandler extends Activity
-        implements MusicUtils.Defs {
+public class GestureHandler extends Activity {
     private static final String GESTURE_CONTROL_PLAY =
            "com.android.music.MusticGesturePlayActivity";
     private static final String GESTURE_CONTROL_PREV =
            "com.android.music.MusticGesturePrevActivity";
     private static final String GESTURE_CONTROL_NEXT =
            "com.android.music.MusticGestureNextActivity";
-    private ServiceToken mToken;
     private static final String LOGTAG = "GestureHandler";
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        mToken = MusicUtils.bindToService(this, autoshuffle);
         Intent intent = getIntent();
         String componentName = intent.getComponent().getClassName();
         Log.d(LOGTAG,"GestureHandler get componentName : "+ componentName);
+
         if (GESTURE_CONTROL_PLAY.equals(componentName)) {
-            Intent i = new Intent("com.android.music.musicservicecommand");
-            i.putExtra("command", "togglepause");
-            this.sendBroadcast(i);
+            Intent i = new Intent(this, MediaPlaybackService.class);
+            i.setAction(MediaPlaybackService.SERVICECMD);
+            i.putExtra(MediaPlaybackService.CMDNAME, MediaPlaybackService.CMDTOGGLEPAUSE);
+            this.startService(i);
         } else if (GESTURE_CONTROL_PREV.equals(componentName)) {
-            Intent i = new Intent("com.android.music.musicservicecommand");
-            i.putExtra("command", "previous");
-            this.sendBroadcast(i);
+            Intent i = new Intent(this, MediaPlaybackService.class);
+            i.setAction(MediaPlaybackService.SERVICECMD);
+            i.putExtra(MediaPlaybackService.CMDNAME, MediaPlaybackService.CMDPREVIOUS);
+            this.startService(i);
         } else if (GESTURE_CONTROL_NEXT.equals(componentName)) {
-            Intent i = new Intent("com.android.music.musicservicecommand");
-            i.putExtra("command", "next");
-            this.sendBroadcast(i);
+            Intent i = new Intent(this, MediaPlaybackService.class);
+            i.setAction(MediaPlaybackService.SERVICECMD);
+            i.putExtra(MediaPlaybackService.CMDNAME, MediaPlaybackService.CMDNEXT);
+            this.startService(i);
         }
         finish();
     };
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        MusicUtils.unbindFromService(mToken);
-    }
-
-    private ServiceConnection autoshuffle = new ServiceConnection() {
-        public void onServiceConnected(ComponentName classname, IBinder obj) {
-            IMediaPlaybackService serv = IMediaPlaybackService.Stub.asInterface(obj);
-            if (serv != null) {
-                try {
-                    serv.setShuffleMode(MediaPlaybackService.SHUFFLE_AUTO);
-                } catch (RemoteException ex) {
-                }
-            }
-        }
-        public void onServiceDisconnected(ComponentName classname) {
-        }
-    };
 }
