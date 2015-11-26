191e89707dc582e7006bd003774b267b56052615
Sumit Bajpai <sbajpai@codeaurora.org>
2015-11-26 17:06:21 +0530

    Bluetooth: Update shuffle mode on DUT when change from BT remote.
    Shuffle mode was not updated on DUT when changed from BT remote
    device due to incorrect conditional logic. The shuffle change
    request was getting ignored in music app always.
    
    CRs-Fixed: 944897
    Change-Id: Icce90319412f11288559eb415c569a9b55fb9911

--- src/com/android/music/MediaPlaybackService.java
+++ src/com/android/music/MediaPlaybackService.java
@@ -1959,12 +1959,16 @@ public class MediaPlaybackService extends Service {
 
     public void setShuffleMode(int shufflemode) {
         synchronized(this) {
-            mShuffleMode = shufflemode;
-            notifyAttributeValues(PLAYERSETTINGS_RESPONSE,
-                            mAttributePairs, SET_ATTRIBUTE_VALUES);
-            if (mShuffleMode == shufflemode && mPlayListLen > 0) {
+            if ((mShuffleMode == shufflemode) && (mPlayListLen > 0)) {
+                /**
+                 * Some carkits send Shuffle Values same as our current
+                 * values. In such cases we need to respond back to ck
+                 */
+                notifyAttributeValues(PLAYERSETTINGS_RESPONSE,
+                                mAttributePairs, SET_ATTRIBUTE_VALUES);
                 return;
             }
+            mShuffleMode = shufflemode;
             if (mShuffleMode == SHUFFLE_AUTO) {
                 if (makeAutoShuffleList()) {
                     mPlayListLen = 0;
@@ -1973,12 +1977,16 @@ public class MediaPlaybackService extends Service {
                     openCurrentAndNext();
                     play();
                     notifyChange(META_CHANGED);
+                    notifyAttributeValues(PLAYERSETTINGS_RESPONSE,
+                            mAttributePairs, SET_ATTRIBUTE_VALUES);
                     return;
                 } else {
                     // failed to build a list of files to shuffle
                     mShuffleMode = SHUFFLE_NONE;
                 }
             }
+            notifyAttributeValues(PLAYERSETTINGS_RESPONSE,
+                            mAttributePairs, SET_ATTRIBUTE_VALUES);
             notifyChange(SHUFFLE_CHANGED);
             saveQueue(false);
         }
